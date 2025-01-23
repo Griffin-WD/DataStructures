28219b78f97b7c045c2decb66018bd8b85a2f136
Griffin-WD <griffin.w.dunlap@gmail.com>
2025-01-23 12:00:12 -0600

    Added Text

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -6,7 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "This is my GitHub Test Code!";
+    std::cout << "This is my GitHub Test Code!\n";
+    std::cout << "New Text";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
